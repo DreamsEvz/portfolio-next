055606f24e0e56c7fff1035ff8e10b114632f9f3
Evan <marcel.evan2001@gmail.com>
2023-11-23 17:44:18 +0100

    init

---
+++ src/app/components/presentation.tsx
@@ -0,0 +1,14 @@
+const Presentation = () => {
+    return (
+        <section className="w-full h-screen flex">
+            <div className="w-1/2 h-full flex justify-center items-center">
+                <h1 className="text-white text-4xl">Evan MARCEL - Développeur Web</h1>
+            </div>
+            <div className="w-1/2 h-full flex justify-center">
+            </div>
+            <div className="absolute"></div>
+        </section>
+    )
+}
+
+export default Presentation;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return <h1>Hello, ceci est ma page de blog!</h1>
+  }
+
